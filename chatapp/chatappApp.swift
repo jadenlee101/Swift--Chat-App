544bf69ed8e7f6b89205099587b600a71821bfc8
jaden <jaden.lee101@gmail.com>
2023-03-23 16:01:14 -0600

    Initial Commit

---
+++ chatapp/chatappApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatappApp.swift
+//  chatapp
+//
+//  Created by Jaden Lee on 2023-03-23.
+//
+
+import SwiftUI
+
+@main
+struct chatappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
