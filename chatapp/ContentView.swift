dbcaf71d3c7f6bd37a873a4472c1cc90cdd452ba
jaden <jaden.lee101@gmail.com>
2023-03-24 02:30:36 -0600

    first commit

--- chatapp/ContentView.swift
+++ chatapp/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text("Hello, world! tests")
-            .padding()
+        
+        
+        NavigationView {
+            ScrollView {
+                VStack{
+                    Picker(selection: $isLoginMode, label: Text("picker here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                        
+                    Button {
+                        
+                    } label: {
+                    Image(systemName: "person.fill")
+                            .font(.system(size: 64))
+                            .padding()
+                    }
+                    
+                    TextField("email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                    SecureField("password", text: $password)
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text ("Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10 )
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                }.padding()
+                
+                
+            }
+            .navigationTitle("Create account")
+        }
+        
     }
 }
 
