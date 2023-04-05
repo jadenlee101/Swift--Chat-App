73ca713ca693ef24350a25d079ea5849be26a22f
Jaden Lee <jaden.lee101@gmail.com>
2023-04-04 19:36:21 -0600

    message screen 1.0

---
+++ chatapp/MainMessages/MainMessagesView.swift
@@ -0,0 +1,78 @@
+//
+//  MainMessagesView.swift
+//  chatapp
+//
+//  Created by Jaden Lee on 2023-04-04.
+//
+
+import SwiftUI
+
+struct MainMessagesView: View {
+    var body: some View {
+        NavigationView{
+            //nav bar
+            VStack{
+                HStack(spacing: 14){
+                    Image(systemName: "person.fill")
+                        .font(.system(size: 34, weight: .heavy))
+                    
+                    VStack (alignment: .leading, spacing: 4){
+                        Text("Username")
+                            .font(.system(size: 24 , weight: .bold))
+                        
+                        HStack {
+                            Text("Online")
+                                .font(.system(size: 14))
+                                .foregroundColor(Color(.lightGray))
+                            Circle()
+                                .foregroundColor(Color(.green))
+                                .frame(width: 10, height: 10)
+                                
+                        }
+                    }
+                    Spacer()
+                    Image(systemName: "gear")
+                }
+                .padding(.horizontal)
+                ScrollView{
+                    VStack{
+                        HStack (spacing: 16){
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 32))
+                                .padding(8)
+                                .overlay(RoundedRectangle(cornerRadius: 44)
+                                    .stroke(Color.black, lineWidth: 1)
+                                )
+                            
+                            VStack (alignment: .leading){
+                                Text("UserName")
+                                Text("message sent to the user")
+                            }
+                            Spacer()
+                            
+                            Text("22d")
+                                .font(.system(size: 14, weight: .semibold))
+                        }
+                        Divider()
+                            .padding(.vertical, 8)
+                    }
+                    .padding(.horizontal)
+                }
+                Button{} label: {
+                    Text("+ New messages")
+                        .background(Color.blue)
+                }
+                
+                
+            }
+            .navigationBarHidden(true)
+            
+        }
+    }
+}
+
+struct MainMessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMessagesView()
+    }
+}
